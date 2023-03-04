905306be12de6ac527656acd53981141cd97be6e
bscoutinho <bscoutinho2012@gmail.com>
2023-03-04 12:54:51 +0100

    product list mobile screen

--- client/src/app/components/product-list/product-list.component.ts
+++ client/src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Product } from 'src/app/interfaces';
 import { ProductService } from 'src/app/services/productsservice';
 import { Router } from '@angular/router';
+import { Observable, startWith, throttleTime } from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
@@ -10,19 +13,26 @@ import { Router } from '@angular/router';
 })
 export class ProductListComponent {
   public products: Product[] = [];
+  public mobile: boolean = false;
+
+  // Listener for window resize event to change mobile variable
+  @HostListener('window:resize', ['$event'])
+	onResize(event: any) {
+    this.mobile = event.target.innerWidth <= 959 ? true : false;
+	}
 
   // inject services
-  constructor(private productService: ProductService, private router: Router) {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
+
+    // Check if mobile when app starts
+    this.mobile = document.body.offsetWidth <= 959 ? true : false;
+
     // Get data from service with subscription
     this.productService.getData().subscribe((data) => {
       this.products = data[575];
       console.log('Retrieving products: ', this.products);
-      // order by res.position
-      /* this.products = res.sort((a: Product, b: Product) => {
-            return a.position - b.position;
-          }); */
     });
   }
 
--- client/src/app/services/productsservice.ts
+++ client/src/app/services/productsservice.ts
@@ -9,17 +9,18 @@ export class ProductService {
 
     constructor(private http: HttpClient) { }
 
+    //http get request
     getData() {
         return this.http.get<any>(this.baseUrl);
     }
 
     saveData(list: Product[]) {
-
+        // set data to be posted
         let body = {
             "575": list
         }
 
-        // post data to server
+        // post data to server partially (patch)
         this.http.patch(this.baseUrl, body).subscribe((data) => {
             console.log('Product list saved: ', data);
         });
